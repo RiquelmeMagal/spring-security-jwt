2e1172a059364aeca539e83b852c74ac2aba6f60
Riquelme Magalhaes <riquelme.souza@arapiraca.ufal.br>
2024-06-07 14:37:58 -0300

    :sparkles: feat: Inicializando o projeto

---
+++ src/main/java/dio/dio_spring_secutiry_jwt/DioSpringSecutiryJwtApplication.java
@@ -0,0 +1,13 @@
+package dio.dio_spring_secutiry_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringSecutiryJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringSecutiryJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/dio_spring_secutiry_jwt/DioSpringSecutiryJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio_spring_secutiry_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecutiryJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
